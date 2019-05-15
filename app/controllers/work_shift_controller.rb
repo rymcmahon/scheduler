5b9d549d291d17905f313fbb7d510478ad0cabc2
rymcmahon <toedragdangle9@gmail.com>
2019-05-14 20:57:44 -0400

    added partials and employee id passes to modal

--- app/controllers/work_shift_controller.rb
+++ app/controllers/work_shift_controller.rb
@@ -1,5 +1,10 @@
 class WorkShiftController < ApplicationController
   def index
     @work_shifts = WorkShift.all
+
+    respond_to do |format|
+      format.html
+      format.js { render partial: 'edit_shift'}
+    end
   end
 end
